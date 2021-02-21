04932886dc480143113475fab8ca74fe2b75a507
amalalanazi <ammal_77@hotmail.com>
2021-02-21 15:36:20 +0300

    TripList.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react'
 import Navs from './Navs';
+import TripList from './TripList';
 
 
 export default class App extends Component {
@@ -14,7 +14,8 @@ export default class App extends Component {
   render() {
     return (
     <div class="body">
-      <Navs />
+      {/* <Navs /> */}
+      <TripList/>
     </div>
     )
   }
