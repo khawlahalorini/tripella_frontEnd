9ef30cbc154e84a0b7534991b713d0456c83b12f
sarahibrahim <sisms.1417@gmail.com>
2021-02-21 13:03:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Login from './user/Login';
-import Profile from './user/Profile';
 import Register from './user/Register'
 
 
@@ -8,7 +7,7 @@ function App() {
   return (
 <div>
 
-<Profile/>
+
 
 </div>
   );
