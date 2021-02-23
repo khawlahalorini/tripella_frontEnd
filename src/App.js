cdcdeba1c4c14ca26f0ee35563a34d9d31a2a1ac
sarahibrahim <sisms.1417@gmail.com>
2021-02-23 17:39:00 +0300

    filter

--- src/App.js
+++ src/App.js
@@ -2,19 +2,40 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react'
 import Navs from './Navs';
+import ARRAY from './ARRAY';
 
 
 
 export default class App extends Component {
-  state = {
-    user: null
-  };
+  constructor(props) {
+    super(props)
+    this.state = {
+      places: ARRAY.places,
+      faves: [],
+      current: {}
+    }
+  }
+
+  handleFaveToggle = (place) => {
+
+    const faves = [...this.state.faves];
+    const placeIndex = faves.indexOf(place)
+
+    if (placeIndex !== -1) {
+      faves.splice(placeIndex, 1);
+      console.log(`Removing ${place.title} From Favors`)
+    } else {
+      faves.push(place);
+      console.log(`Adding ${place.title} To Favors`)
+    }
+    this.setState({ faves })
+  }
 
   
   render() {
     return (
     <div class="body">
-      <Navs />
+      <Navs  handleDetailsClick={this.handleDetailsClick} films={this.state.films} faves={this.state.faves} onFaveToggle={this.handleFaveToggle} />
      
     
     </div>
