0039b060e8229b271894794e9f1a2f81b5655b93
sarahibrahim <sisms.1417@gmail.com>
2021-02-25 13:07:10 +0300

    .

--- src/user/ProfileEditForm.js
+++ src/user/ProfileEditForm.js
@@ -1,23 +1,12 @@
 import React, { Component } from 'react'
-import { Card, ListGroupItem, ListGroup } from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
 
 export default class ProfileEditForm extends Component {
 
     render() {
         return (
             <div>
-
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="https://psgpharma.ac.in/wp-content/uploads/2019/02/empty-img.jpg" />
-                    <Card.Body>
-                        <Card.Title>username</Card.Title>
-                        {/* <Card.Title>{this.state.username}</Card.Title> */}
-                        <Card.Text>Email</Card.Text>
-                        {/* <Card.Text>{this.state.emailAddress}</Card.Text> */}
-                        <Card.Text>Bio</Card.Text>
-                    </Card.Body>
-                </Card>
-                {/* <form
+                <Form
                 //  onSubmit={this.handleSubmit}
                 >
                     <div>
@@ -37,7 +26,7 @@ export default class ProfileEditForm extends Component {
                         // onChange={this.handleChange}
                         ></input>
                     </div>
-                    {/* <div>
+                    <div>
                         <Form.File
                             className="position-relative"
                             required
@@ -46,11 +35,11 @@ export default class ProfileEditForm extends Component {
                             id="validationFormik107"
                             feedbackTooltip
                         />
-                    </div> */}
-                {/* <div>
+                    </div>
+                <div>
                         <input type="submit" value="Save"></input>
                     </div>
-                </form> */}
+                </Form>
 
             </div>
         )
