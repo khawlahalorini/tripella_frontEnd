cd4be81dada02c47fce38092943f27eb4d0daa74
amalalanazi <ammal_77@hotmail.com>
2021-03-02 15:50:50 +0300

    solved conf

--- src/component/PlaceInfo.js
+++ src/component/PlaceInfo.js
@@ -8,19 +8,12 @@ export default class PlaceInfo extends Component {
             <div>
                 <div class="w3-card-4" style={{width:"40%" , margin:"20px"}}>
                     <header class="w3-container w3-light-grey">
-<<<<<<< HEAD
-                        <h3>{this.props.wish.title}</h3>
-                        </header>
-                        <div class="w3-container">
-                            <PlacesPIc wish={this.props.wish} /> 
-                            <p>{this.props.wish.overview}</p>
-=======
+
                         <h3>{this.props.place.title}</h3>
                         </header>
                         <div class="w3-container">
                             <PlacesPIc places={this.props.place} /> 
                             <p>{this.props.place.overview}</p>
->>>>>>> 3c20f8aaa647061f9ab5d9fc68c3010f23f0eb24
                             <div class="w3-container">
                                 <button class="w3-button w3-xlarge w3-circle w3-grey w3-right"  style={{marginBottom:"10px"}}>+</button>
                             </div>
