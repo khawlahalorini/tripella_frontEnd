971f243b8923d9022c0d4955b22f444700c743b6
sarahibrahim <sisms.1417@gmail.com>
2021-03-03 22:53:42 +0300

    edit card home

--- src/component/HomeCard.js
+++ src/component/HomeCard.js
@@ -12,7 +12,7 @@ export default class HomeCard extends Component {
 
             <div class="w3-card-4 w3-margin w3-col w3-white w3-round-large" style={{width:"22.5%"}}>
                 <div class="w3-display-container w3-text-white ">
-                    <img src={this.props.places.poster_path}  alt="Lights" style={{width:"100%"}} />
+                    <img src={this.props.places.poster_path}  style={{width:"100%",height:"50%"}} />
                     <div class="w3-xlarge w3-display-bottomleft w3-padding">{this.props.places.title}</div>
                 </div>
                     <div class=" w3-center ">
